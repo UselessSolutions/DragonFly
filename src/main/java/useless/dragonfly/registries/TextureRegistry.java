81202ce45616f2e6d0a41a9a5ec8164fdc49c3e9
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-11-17 20:07:19 -0600

    registered all block textures

--- src/main/java/useless/dragonfly/registries/TextureRegistry.java
+++ src/main/java/useless/dragonfly/registries/TextureRegistry.java
@@ -48,7 +48,7 @@ public class TextureRegistry {
 		registerVanillaBlockTexture("grass_top", 0, 0);
 		registerVanillaBlockTexture("cobbled_stone", 0, 1);
 		registerVanillaBlockTexture("moss_overlay", 0, 2);
-		registerVanillaBlockTexture("sponge_dry", 0, 3);
+		registerVanillaBlockTexture("sponge", 0, 3);
 		registerVanillaBlockTexture("torch_coal", 0, 5);
 		registerVanillaBlockTexture("lever_cobble_stone", 0, 6);
 		registerVanillaBlockTexture("rail_turn", 0, 7);
@@ -231,7 +231,7 @@ public class TextureRegistry {
 		registerVanillaBlockTexture("debug_cyan", 6, 31);
 
 		registerVanillaBlockTexture("brick_clay", 7, 0);
-		registerVanillaBlockTexture("grass_tall", 7, 2);
+		registerVanillaBlockTexture("tallgrass", 7, 2);
 		registerVanillaBlockTexture("deadbush", 7, 3);
 		registerVanillaBlockTexture("cactus_bottom", 7, 4);
 		registerVanillaBlockTexture("farmland_dry_top", 7, 5);
@@ -242,7 +242,7 @@ public class TextureRegistry {
 		registerVanillaBlockTexture("cobbled_basalt", 7, 10);
 		registerVanillaBlockTexture("spike_top", 7, 11);
 		registerVanillaBlockTexture("motionsensor_front_idle", 7, 12);
-		registerVanillaBlockTexture("slate", 7, 13);
+		registerVanillaBlockTexture("slate_side", 7, 13);
 		registerVanillaBlockTexture("polished_slate", 7, 14);
 		registerVanillaBlockTexture("animation_breaking_7", 7, 15);
 		registerVanillaBlockTexture("pebbles_overlay0", 7, 18);
@@ -250,5 +250,193 @@ public class TextureRegistry {
 		registerVanillaBlockTexture("fence_chainlink_pole", 7, 20);
 		registerVanillaBlockTexture("debug_magenta", 7, 31);
 
+		registerVanillaBlockTexture("tnt_side", 8, 0);
+		registerVanillaBlockTexture("grass_retro_top", 8, 1);
+		registerVanillaBlockTexture("grass_top2", 8, 2); // Unused?
+		registerVanillaBlockTexture("fern", 8, 3);
+		registerVanillaBlockTexture("block_clay", 8, 4);
+		registerVanillaBlockTexture("crops_wheat_stage0", 8, 5);
+		registerVanillaBlockTexture("soulsand", 8, 6);
+		registerVanillaBlockTexture("pumpkin_carved_active", 8, 7);
+		registerVanillaBlockTexture("brick_iron", 8, 8);
+		registerVanillaBlockTexture("bed_top_back", 8, 9);
+		registerVanillaBlockTexture("brick_basalt", 8, 10);
+		registerVanillaBlockTexture("mesh", 8, 11);
+		registerVanillaBlockTexture("motionsensor_front_active", 8, 12);
+		registerVanillaBlockTexture("slate_top", 8, 13);
+		registerVanillaBlockTexture("carved_slate_top", 8, 14);
+		registerVanillaBlockTexture("animation_breaking_8", 8, 15);
+		registerVanillaBlockTexture("pebbles_overlay1", 8, 18);
+		registerVanillaBlockTexture("fence_chainlink_top", 8, 19);
+		registerVanillaBlockTexture("debug_yellow", 8, 31);
+
+		registerVanillaBlockTexture("tnt_top", 9, 0);
+		registerVanillaBlockTexture("chest_planks_oak_top", 9, 1);
+		registerVanillaBlockTexture("chest_planks_oak_double_front_left", 9, 2);
+		registerVanillaBlockTexture("chest_planks_oak_double_back_left", 9, 3);
+		registerVanillaBlockTexture("sugarcane", 9, 4);
+		registerVanillaBlockTexture("crops_wheat_stage1", 9, 5);
+		registerVanillaBlockTexture("glowstone", 9, 6);
+		registerVanillaBlockTexture("cake_top", 9, 7);
+		registerVanillaBlockTexture("brick_lapis", 9, 8);
+		registerVanillaBlockTexture("brick_gold", 9, 9);
+		registerVanillaBlockTexture("limestone", 9, 10);
+		registerVanillaBlockTexture("granite", 9, 11);
+		registerVanillaBlockTexture("marble", 9, 12);
+		registerVanillaBlockTexture("capstone_marble_side", 9, 13);
+		registerVanillaBlockTexture("capstone_marble_top", 9, 14);
+		registerVanillaBlockTexture("animation_breaking_9", 9, 15);
+		registerVanillaBlockTexture("pebbles_overlay2", 9, 18);
+
+		registerVanillaBlockTexture("tnt_bottom", 10, 0);
+		registerVanillaBlockTexture("chest_planks_oak_side", 10, 1);
+		registerVanillaBlockTexture("chest_planks_oak_front_right", 10, 2);
+		registerVanillaBlockTexture("chest_planks_oak_back_right", 10, 3);
+		registerVanillaBlockTexture("noteblock", 10, 4);
+		registerVanillaBlockTexture("crops_wheat_stage2", 10, 5);
+		registerVanillaBlockTexture("piston_head_front_sticky", 10, 6);
+		registerVanillaBlockTexture("cake_side", 10, 7);
+		registerVanillaBlockTexture("brick_stone", 10, 8);
+		registerVanillaBlockTexture("ore_nethercoal_netherrack", 10, 9);
+		registerVanillaBlockTexture("cobbled_limestone", 10, 10);
+		registerVanillaBlockTexture("cobbled_granite", 10, 11);
+		registerVanillaBlockTexture("motionsensor_side", 10, 12);
+		registerVanillaBlockTexture("brick_marble", 10, 13);
+		registerVanillaBlockTexture("carved_marble_side", 10, 14); // unused
+		registerVanillaBlockTexture("ice_perma", 10, 15);
+
+		registerVanillaBlockTexture("cobweb", 11, 0);
+		registerVanillaBlockTexture("chest_planks_oak_single_front", 11, 1);
+		registerVanillaBlockTexture("workbench_top", 11, 2);
+		registerVanillaBlockTexture("workbench_front", 11, 3);
+		registerVanillaBlockTexture("jukebox_top", 11, 4);
+		registerVanillaBlockTexture("crops_wheat_stage3", 11, 5);
+		registerVanillaBlockTexture("piston_head_front", 11, 6);
+		registerVanillaBlockTexture("cake_inner", 11, 7);
+		registerVanillaBlockTexture("brick_stone_mossy", 11, 8);
+		registerVanillaBlockTexture("sponge_wet", 11, 9);
+		registerVanillaBlockTexture("brick_limestone", 11, 10);
+		registerVanillaBlockTexture("brick_granite", 11, 11);
+		registerVanillaBlockTexture("column_marble_side", 11, 12);
+		registerVanillaBlockTexture("column_marble_top", 11, 13);
+		registerVanillaBlockTexture("carved_marble_top", 11, 14); // unused
+		registerVanillaBlockTexture("permafrost", 11, 15);
+
+		registerVanillaBlockTexture("flower_red", 12, 0);
+		registerVanillaBlockTexture("mushroom_red", 12, 1);
+		registerVanillaBlockTexture("furnace_stone_idle_front", 12, 2);
+		registerVanillaBlockTexture("workbench_side", 12, 3);
+		registerVanillaBlockTexture("netherrack_igneous", 12,4);
+		registerVanillaBlockTexture("crops_wheat_stage4", 12,5);
+		registerVanillaBlockTexture("piston_side", 12,6);
+		registerVanillaBlockTexture("cake_bottom", 12,7);
+		registerVanillaBlockTexture("dirt_scorched_rich", 12,8);
+		registerVanillaBlockTexture("brick_stone_polished", 12,9);
+		registerVanillaBlockTexture("spinifex", 12,10);
+		registerVanillaBlockTexture("block_olivine", 12,11);
+		registerVanillaBlockTexture("block_quartz", 12,12);
+		registerVanillaBlockTexture("stool_side", 12,13);
+		registerVanillaBlockTexture("cobbled_permafrost", 12,15);
+
+		registerVanillaBlockTexture("flower_yellow", 13,0);
+		registerVanillaBlockTexture("mushroom_brown", 13,1);
+		registerVanillaBlockTexture("furnace_stone_side", 13,2);
+		registerVanillaBlockTexture("furnace_stone_active_front", 13,3);
+		registerVanillaBlockTexture("soulslate", 13,4); // making up a name no clue what this is
+		registerVanillaBlockTexture("crops_wheat_stage5", 13,5);
+		registerVanillaBlockTexture("piston_back", 13,6);
+		registerVanillaBlockTexture("furnace_blast_idle_front", 13,7);
+		registerVanillaBlockTexture("dirt_scorched", 13,8);
+		registerVanillaBlockTexture("debug_transparent3", 13,9);
+		registerVanillaBlockTexture("lantern_firefly_blue", 13,10);
+		registerVanillaBlockTexture("lantern_firefly_green", 13,11);
+		registerVanillaBlockTexture("water0", 13,12);
+		registerVanillaBlockTexture("stool_top", 13,13);
+		registerVanillaBlockTexture("lava0", 13,14);
+		registerVanillaBlockTexture("brick_permafrost", 13,15);
+
+		registerVanillaBlockTexture("debug_blue2", 14,0);
+		registerVanillaBlockTexture("dispenser_cobble_stone_front", 14,2);
+		registerVanillaBlockTexture("furnace_stone_top", 14,3);
+		registerVanillaBlockTexture("netherrack_bloody", 14,4);
+		registerVanillaBlockTexture("crops_wheat_stage6", 14,5);
+		registerVanillaBlockTexture("piston_front_extended", 14,6);
+		registerVanillaBlockTexture("furnace_blast_side", 14,7);
+		registerVanillaBlockTexture("furnace_blast_active_front", 14,8);
+		registerVanillaBlockTexture("furnace_blast_bottom", 14,9);
+		registerVanillaBlockTexture("lantern_firefly_orange", 14,10);
+		registerVanillaBlockTexture("lantern_firefly_red", 14,11);
+		registerVanillaBlockTexture("water1", 14,12);
+		registerVanillaBlockTexture("water2", 14,13);
+		registerVanillaBlockTexture("lava1", 14,14);
+		registerVanillaBlockTexture("lava2", 14,15);
+
+		registerVanillaBlockTexture("debug_fire", 15,1);
+		registerVanillaBlockTexture("debug_fire2", 15,2);
+		registerVanillaBlockTexture("crops_wheat_stage7", 15,5);
+		registerVanillaBlockTexture("block_green_diamond", 15,6);
+		registerVanillaBlockTexture("block_charcoal", 15,7);
+		registerVanillaBlockTexture("furnace_blast_top", 15,8);
+		registerVanillaBlockTexture("lantern_inside", 15,11);
+		registerVanillaBlockTexture("water3", 15,12);
+		registerVanillaBlockTexture("water4", 15,13);
+		registerVanillaBlockTexture("lava3", 15,14);
+		registerVanillaBlockTexture("lava4", 15,15);
+		registerVanillaBlockTexture("debug_transparent4", 15,16);
+
+		registerVanillaBlockTexture("ore_gold_stone", 16,0);
+		registerVanillaBlockTexture("ore_gold_basalt", 16,1);
+		registerVanillaBlockTexture("ore_gold_limestone", 16,2);
+		registerVanillaBlockTexture("ore_gold_granite", 16,3);
+		registerVanillaBlockTexture("block_iron_top", 16,4);
+		registerVanillaBlockTexture("block_iron_side", 16,5);
+		registerVanillaBlockTexture("block_iron_bottom", 16,6);
+		registerVanillaBlockTexture("block_coal", 16,7);
+		registerVanillaBlockTexture("debug_north", 16,8);
+		registerVanillaBlockTexture("debug_east", 16,9);
+		registerVanillaBlockTexture("grass_scorched_top", 16,11);
+
+		registerVanillaBlockTexture("ore_iron_stone", 17,0);
+		registerVanillaBlockTexture("ore_iron_basalt", 17,1);
+		registerVanillaBlockTexture("ore_iron_limestone", 17,2);
+		registerVanillaBlockTexture("ore_iron_granite", 17,3);
+		registerVanillaBlockTexture("block_gold_top", 17,4);
+		registerVanillaBlockTexture("block_gold_side", 17,5);
+		registerVanillaBlockTexture("block_gold_bottom", 17,6);
+		registerVanillaBlockTexture("block_nethercoal", 17,7);
+		registerVanillaBlockTexture("debug_south", 17,8);
+		registerVanillaBlockTexture("debug_west", 17,9);
+		registerVanillaBlockTexture("grass_scorched_side", 17,11);
+
+		registerVanillaBlockTexture("ore_coal_stone", 18,0);
+		registerVanillaBlockTexture("ore_coal_basalt", 18,1);
+		registerVanillaBlockTexture("ore_coal_limestone", 18,2);
+		registerVanillaBlockTexture("ore_coal_granite", 18,3);
+		registerVanillaBlockTexture("block_diamond_top", 18,4);
+		registerVanillaBlockTexture("block_diamond_side", 18,5);
+		registerVanillaBlockTexture("block_diamond_bottom", 18,6);
+		registerVanillaBlockTexture("debug_down", 18,8);
+		registerVanillaBlockTexture("debug_up", 18,9);
+
+		registerVanillaBlockTexture("ore_lapis_stone", 19,0);
+		registerVanillaBlockTexture("ore_lapis_basalt", 19,1);
+		registerVanillaBlockTexture("ore_lapis_limestone", 19,2);
+		registerVanillaBlockTexture("ore_lapis_granite", 19,3);
+		registerVanillaBlockTexture("block_steel_top", 19,4);
+		registerVanillaBlockTexture("block_steel_side", 19,5);
+		registerVanillaBlockTexture("block_steel_bottom", 19,6);
+
+		registerVanillaBlockTexture("ore_diamond_stone", 20,0);
+		registerVanillaBlockTexture("ore_diamond_basalt", 20,1);
+		registerVanillaBlockTexture("ore_diamond_limestone", 20,2);
+		registerVanillaBlockTexture("ore_diamond_granite", 20,3);
+
+		registerVanillaBlockTexture("ore_redstone_stone", 21,0);
+		registerVanillaBlockTexture("ore_redstone_basalt", 21,1);
+		registerVanillaBlockTexture("ore_redstone_limestone", 21,2);
+		registerVanillaBlockTexture("ore_redstone_granite", 21,3);
+
+		registerVanillaBlockTexture("mud", 22,0);
+		registerVanillaBlockTexture("mud_dry", 23,0);
 	}
 }
