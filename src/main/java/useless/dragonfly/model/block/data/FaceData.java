cd8c83a01f561fa59ead3248fa6db96a000f6871
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-11-18 19:14:07 -0600

    Fully model loading

--- src/main/java/useless/dragonfly/model/block/data/FaceData.java
+++ src/main/java/useless/dragonfly/model/block/data/FaceData.java
@@ -4,9 +4,13 @@ import com.google.gson.annotations.SerializedName;
 
 public class FaceData {
 	@SerializedName("uv")
-	public int[] uv = new int[0];
+	public int[] uv = new int[4];
 	@SerializedName("texture")
 	public String texture = null;
+	@SerializedName("cullface")
+	public String cullface = null;
+	@SerializedName("rotation")
+	public int rotation = 0; // can only be values 0, 90, 180, or 270
 	@SerializedName("tintindex")
 	public int tintindex = -1; // Should mean white tint, unsure how to handle other values seems to pass it into BlockColor to get a color from there in vanilla
 
