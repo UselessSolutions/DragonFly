01781ac4f7531850feee4b63e24ccacaf1e02c2b
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-11-19 18:20:24 -0600

    Removed test assets from github

--- src/main/java/useless/dragonfly/debug/DebugEntities.java
+++ src/main/java/useless/dragonfly/debug/DebugEntities.java
@@ -10,6 +10,6 @@ import static useless.dragonfly.DragonFly.MOD_ID;
 
 public class DebugEntities {
 	public static void init(){
-		EntityHelper.createEntity(EntityHTest.class, new RenderHTest(ModelHelper.getOrCreateEntityModel(MOD_ID, "hierachyTest.json", HModelTest.class), 0.5f), 1000, "ht");
+		//EntityHelper.createEntity(EntityHTest.class, new RenderHTest(ModelHelper.getOrCreateEntityModel(MOD_ID, "hierachyTest.json", HModelTest.class), 0.5f), 1000, "ht");
 	}
 }
