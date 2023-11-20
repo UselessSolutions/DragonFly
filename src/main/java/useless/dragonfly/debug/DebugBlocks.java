a9b1b53fd2ee97b2f7e26437671479cda896ffd2
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-11-19 20:29:43 -0600

    Made BlockFace UVs consistent

--- src/main/java/useless/dragonfly/debug/DebugBlocks.java
+++ src/main/java/useless/dragonfly/debug/DebugBlocks.java
@@ -36,6 +36,14 @@ public static final Block testBlock = new BlockBuilder(MOD_ID)
 		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(MOD_ID, "block/harris.json")))
 		.setTextures(1,0)
 		.build(new BlockModel("testblock" + blockId, blockId++, Material.dirt, ModelHelper.getOrCreateBlockModel(MOD_ID, "block/harris.json")));
+	public static final Block lantern = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(TextureRegistry.coreNamepaceId, "block/lantern.json")))
+		.setTextures(1,0)
+		.build(new BlockModel("testblock" + blockId, blockId++, Material.dirt, ModelHelper.getOrCreateBlockModel(TextureRegistry.coreNamepaceId, "block/lantern.json")));
+	public static final Block lanternsould = new BlockBuilder(MOD_ID)
+		.setBlockModel(new BlockModelDragonFly(ModelHelper.getOrCreateBlockModel(TextureRegistry.coreNamepaceId, "block/soul_lantern.json")))
+		.setTextures(1,0)
+		.build(new BlockModel("testblock" + blockId, blockId++, Material.dirt, ModelHelper.getOrCreateBlockModel(TextureRegistry.coreNamepaceId, "block/soul_lantern.json")));
 
 	public static void init() {}
 }
